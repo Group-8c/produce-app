8ab6032e82ecc83a2c923e703a15087313b35b91
GraceBenson <gracebenson@ufl.edu>
2019-10-29 20:54:57 -0400

    built basic navbar design

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,12 @@ import { Route, Switch, Redirect  } from 'react-router-dom';
 import Home from "./views/Home/Home"
 import NotFound from "./views/NotFound"
 import Header from "./components/Header/Header"
-
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
     <div>
+      <Navbar />
       <Header />
       <Switch>
         <Route exact path="/Home" component={Home} />
---
+++ client/src/components/Navbar.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Navbar, Nav } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+import Searchbar from './Searchbar'
+import logo1 from './images/logo1.png'
+import './style.css'
+
+export default () => {
+    return (
+        <Navbar bg="light" expand="lg">
+                <Navbar.Brand><Link id="logo-link" to="/"><img className="topnav-logo" width="65" src={logo1} /></Link></Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="navbar-nav ml-auto order-2">
+                        <Searchbar />
+                        <Link className="topnav-link" class="nav-link" to='/Signin'>Sign in <span class="sr-only">(current)</span></Link>
+                        <Link className="topnav-link" class="nav-link" to='/Register'>Register <span class="sr-only">(current)</span></Link>
+                    </Nav>
+                    <Nav className="navbar-nav mr-auto order-1">
+                        <Link className="topnav-link" class="nav-link" to='/Home'>Home <span class="sr-only">(current)</span></Link>
+                        <Link className="topnav-link" class="nav-link" to='/About'>About <span class="sr-only">(current)</span></Link>
+                        <Link className="topnav-link" class="nav-link" to='/Services'>Services <span class="sr-only">(current)</span></Link>
+                        <Link className="topnav-link" class="nav-link" to='/Contact'>Contact <span class="sr-only">(current)</span></Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+    )
+}
