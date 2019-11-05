94bd8d141e64104e6fa80d8de8208ed6225a79e4
Dustin Durden <dkdurden@ufl.edu>
2019-11-04 21:35:00 -0500

    Edited navbar links to go to separate pages, Added a basic featured items component

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Route, Switch, Redirect  } from 'react-router-dom';
 import Home from "./views/Home/Home"
+import About from "./views/About/About"
+import Services from "./views/Services/Services"
+import Contact from "./views/Contact/Contact"
 import NotFound from "./views/NotFound"
 //import Header from "./components/Header/Header"
 import Navbar from './components/Navbar'
@@ -11,8 +14,14 @@ const App = () => {
   return (
     <div>
       <Navbar />
+
+      {//react-router links to static pages
+      }
       <Switch>
         <Route exact path="/Home" component={Home} />
+        <Route path="/About" component={About} />
+        <Route path="/Services" component={Services} />
+        <Route path="/Contact" component={Contact} />
         <Route path="/Register" component={Register} />
         <Route path="/Signin" component={SignIn} />
         <Route exact path="/">
---
+++ client/src/views/Home/featItems.js
@@ -0,0 +1,52 @@
+/*
+    This file holds static data for testing the featured items component.
+*/
+
+import orange from '../../assets/orange.jpg';
+import apple from '../../assets/apple.jpg';
+import bananas from '../../assets/bananas.jpg';
+
+export default [
+    {
+        index: 0,
+        id: 1, 
+        name: 'Oranges',
+        description: 'The best oranges. 5.99', 
+        img: orange
+    },
+    {
+        index: 1, 
+        id: 2, 
+        name: 'Apples',
+        description: 'Fresh apples. 7.99', 
+        img: apple
+    },
+    {
+        index: 2, 
+        id: 3, 
+        name: 'Bananas',
+        description: 'Tasty bananas. 8.99/lb', 
+        img: bananas
+    },
+    {
+        index: 3,
+        id: 4, 
+        name: 'Oranges',
+        description: 'The best oranges. 5.99', 
+        img: orange
+    },
+    {
+        index: 4, 
+        id: 5, 
+        name: 'Apples',
+        description: 'Fresh apples. 7.99', 
+        img: apple
+    },
+    {
+        index: 5, 
+        id: 6, 
+        name: 'Bananas',
+        description: 'Tasty bananas. 8.99/lb', 
+        img: bananas
+    }
+]
