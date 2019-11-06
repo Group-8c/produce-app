04837a1a547faad51489c0947abe0cd531994c7d
Dustin Durden <dkdurden@ufl.edu>
2019-11-06 14:20:59 -0500

    moved some files and deleted unused imports

--- client/src/views/Home/Home.js
+++ client/src/views/Home/Home.js
@@ -1,11 +1,4 @@
 import React from 'react';
-//import logo from '../../assets/logo.svg';
-//import { Jumbotron } from 'react-bootstrap'
-//import orchard from '../../components/images/agriculture-beetroot-beets-533360.jpg'
-//import { Image } from 'react-bootstrap'
-//import Services from '../Services/Services'
-//import About from '../About/About'
-//import Contact from '../../Footer'
 import './Home.css';
 import Carousel from './Carousel';
 
