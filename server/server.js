76312d48fbd350767cca142b11071cbbbf69bba4
Dustin Durden <dkdurden@ufl.edu>
2019-12-03 12:37:27 -0500

    fixed server.js

--- server/server.js
+++ server/server.js
@@ -9,7 +9,9 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
 dotenv.config() 
-const app = express();
+
+//const app = express();
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
