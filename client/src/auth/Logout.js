ad38fdd1861d0c465a5c188f979af3794773658c
Dustin Durden <23153218+dkdurden@users.noreply.github.com>
2019-12-08 17:16:51 -0500

    Update Logout.js

--- client/src/auth/Logout.js
+++ client/src/auth/Logout.js
@@ -4,7 +4,8 @@ import { Link } from 'react-router-dom';
 import { logout } from '../actions/authActions';
 import PropTypes from 'prop-types';
 
-
+// User logout button component. The button is in the user dropdown menu and links to the
+// home screen upon logout.
 export class Logout extends Component {
 
     static propTypes = {
